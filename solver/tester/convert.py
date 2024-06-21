2877d5437bbbc055e27ff41bf627771178ac4b32
sota <sota1210nittc@gmail.com>
2024-06-21 23:19:26 +0900

    全画像をtxtに変換するプログラムの作成

--- solver/tester/convert.py
+++ solver/tester/convert.py
@@ -1,47 +1,31 @@
 import cv2
 import numpy as np
-import json
 
 
-H, W = 32, 32
 K = 4
 
-image_pre = cv2.imread("images/image.jpg")
-if image_pre is None:
-  print("Error")
-  exit()
+# return (出力画像, K個のカラーコード配列, 0~K-1のHW盤面)
+def convert(input_image: np.ndarray, H: int, W: int):
+  if input_image is None:
+    print("Error")
+    return
 
-image_pre = cv2.resize(image_pre, (W, H), interpolation=cv2.INTER_NEAREST)
-image = image_pre.reshape(-1, 3)
-image = image.astype(np.float32)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+  image_pre = cv2.resize(input_image, (W, H), interpolation=cv2.INTER_NEAREST)
+  image = image_pre.reshape(-1, 3)
+  image = image.astype(np.float32)
+  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
-ret, label, center = cv2.kmeans(image, K, None, criteria, 10, cv2.KMEANS_PP_CENTERS)
+  ret, label, center = cv2.kmeans(image, K, None, criteria, 10, cv2.KMEANS_PP_CENTERS)
 
-center = center.astype(np.uint8)
-result = center[label.flatten()]
-result = result.reshape((image_pre.shape))
+  center = center.astype(np.uint8)
+  result = center[label.flatten()]
+  result = result.reshape((image_pre.shape))
 
-r, g, b = cv2.split(result)
-a = np.ones(r.shape, dtype=np.uint8) * 255
-a[0, 0] = 0
-result = cv2.merge((r, g, b, a))
+  r, g, b = cv2.split(result)
+  a = np.ones(r.shape, dtype=np.uint8) * 255
+  a[0, 0] = 0
+  result = cv2.merge((r, g, b, a))
 
-
-# 結果の出力
-cv2.imwrite("result.jpg", result)
-
-with open("input.txt", "w") as f:  
   board = label.flatten().reshape((W, H))
-  for i in range(H):
-    s = ""
-    for j in range(W):
-      s += str(board[i, j])
-    f.write(s + "\n")
 
-with open("color.json", "w") as f:
-  d = {}
-  d["pieceColors"] = []
-  for i in range(K):
-    d["pieceColors"].append(f"({center[i][0]}, {center[i][1]}, {center[i][2]}, 255)")
-  f.write(json.dumps(d, indent=2))
+  return (result, center, board)
---
+++ solver/tester/gen.py
@@ -0,0 +1,54 @@
+import cv2
+import json
+import glob
+import os
+import sys
+import convert
+import numpy as np
+
+
+def write_data(center: np.ndarray, board: np.ndarray, H: int, W: int, outputfile: str, colorfile: str):
+  assert colorfile.endswith(".json"), "colorfileの拡張子をjsonにしてください"
+
+  with open(outputfile, "w") as f:
+    for i in range(H):
+      s = ""
+      for j in range(W):
+        s += str(board[i, j])
+      f.write(s + "\n")
+
+  with open(colorfile, "w") as f:
+    d = {}
+    d["pieceColors"] = []
+    for i in range(convert.K):
+      d["pieceColors"].append(f"({center[i][0]}, {center[i][1]}, {center[i][2]}, 255)")
+    f.write(json.dumps(d, indent=2))
+
+
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print("Argument Error: python3 gen.py H W")
+    exit()
+  if not sys.argv[1].isdigit() or not sys.argv[2].isdigit():
+    print("H or W is not integer")
+
+  H = int(sys.argv[1])
+  W = int(sys.argv[2])
+  dir = "output"
+  os.makedirs(dir, exist_ok=True)
+
+  image_names = glob.glob("images/*.jpg") + glob.glob("images/*.png")
+  print("start writing")
+
+  for name in image_names:
+    file = name.split("/")[-1].split(".")[0]
+    filename = f"{dir}/{H}-{W}--{file}.txt"
+    colorname = f"{dir}/{H}-{W}--{file}-col.json"
+    # 生成済みであればスルー
+    if os.path.exists(filename):
+      continue
+
+    image = cv2.imread(name)
+    result, center, board = convert.convert(image, H, W)
+
+    write_data(center, board, H, W, filename, colorname)
